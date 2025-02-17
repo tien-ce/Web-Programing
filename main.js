fc3b4b4aca736c79d1627f8feff375f30053f431
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2025-02-17 16:34:15 +0700

    change 4:34 - 17

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("xin chao");
+function togglePump(state) {
+  alert(state ? "Bơm nước đã bật!" : "Bơm nước đã tắt!");
+}
+var your_name = prompt("Nhap ten cua ban");
+console.log("Ten cua ban la " + your_name);
